f687e8489bd47d8972d4b6fd94f8aeaa0e810156
0leks <yurchenko.oleksandr.89@gmail.com>
2017-04-06 18:13:28 +0300

    Add login/logout , facebook login , registration account

--- client/main.js
+++ client/main.js
@@ -1,4 +1,7 @@
 import "../imports/startup/client/index";
+import '../imports/startup/accounts-config.js';
+import App from '../imports/ui/App.jsx';
+
 import '/node_modules/ag-grid/dist/styles/ag-grid.css';
 import '/node_modules/ag-grid/dist/styles/theme-fresh.css';
 import '/node_modules/ag-grid/dist/styles/theme-blue.css';
