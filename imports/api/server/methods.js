c318dc80c5c109c1d7a2988cc8ce6d32811bc1c2
dmytrotolbatov <dmytrotolbatov@gmail.comg>
2017-04-14 20:46:41 +0300

    admin pages

--- imports/api/server/methods.js
+++ imports/api/server/methods.js
@@ -1,4 +1,5 @@
-import {Cards} from '../lib/collections';
+import {Cards, Outlets} from '../lib/collections';
+import {Meteor} from 'meteor/meteor';
 
 Meteor.startup(() => {
     Meteor.methods({
@@ -8,8 +9,26 @@ Meteor.startup(() => {
         createCard(dataObj) {
             Cards.insert(dataObj);
         },
+        createOutlets(dataObj) {
+            Outlets.insert(dataObj);
+        },
+        removeOutlets(dataObj) {
+            Outlets.remove(dataObj);
+        },
+        updateOutlets(dataObj) {
+            Outlets.update(dataObj._id, {$set:dataObj});
+        },
         updateCard(dataObj) {
             Cards.update(dataObj._id, {$set:dataObj});
+        },
+        createPerson(dataObj) {
+            Accounts.createUser(dataObj);
+        },
+        removePerson(dataObj) {
+            Meteor.users.remove(dataObj);
+        },
+        updatePerson(dataObj) {
+            Meteor.users.update(dataObj._id, {$set:dataObj});
         }
     });
 });
