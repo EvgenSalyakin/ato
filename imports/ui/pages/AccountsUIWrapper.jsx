32797432ac9f1d1e32ed64ff2673ca44aec303d8
Evgen <e.salyakin@gmail.com>
2017-05-01 20:19:11 +0300

    Evgen: commit Blaze

--- imports/ui/pages/AccountsUIWrapper.jsx
+++ imports/ui/pages/AccountsUIWrapper.jsx
@@ -1,19 +1,19 @@
-import  React , {Component} from 'react';
-import  ReactDom            from 'react-dom';
-import {Template}           from 'meteor/templating';
-import {Blaze}              from 'meteor/blaze';
-
-export default class AccountsUIWrapper extends Component{
-  componentDidMount(){
-    this.view=Blaze.render(Template.loginButtons,
-      ReactDom.findDOMNode(this.refs.container));
-    }
-    componentWillUnmount() {
-      // Clean up Blaze view
-      Blaze.remove(this.view);
-    }
-    render() {
-      // Just render a placeholder container that will be filled in
-      return <span ref="container" />;
-    }
-  }
+// import  React , {Component} from 'react';
+// import  ReactDom            from 'react-dom';
+// import {Template}           from 'meteor/templating';
+// import {Blaze}              from 'meteor/blaze';
+//
+// export default class AccountsUIWrapper extends Component{
+//   componentDidMount(){
+//     this.view=Blaze.render(Template.loginButtons,
+//       ReactDom.findDOMNode(this.refs.container));
+//     }
+//     componentWillUnmount() {
+//       // Clean up Blaze view
+//       Blaze.remove(this.view);
+//     }
+//     render() {
+//       // Just render a placeholder container that will be filled in
+//       return <span ref="container" />;
+//     }
+//   }
